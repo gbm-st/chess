c1cb71ea9a5572d3de6736cc553fa223452d3954
Citrina <Citrina.works@gmail.com>
2022-11-04 07:48:24 -0700

    Se actualizó el movimiento del Rey.

--- src/Rey.java
+++ src/Rey.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Rey extends Pieza{
-    public int si;
     public Rey(int simbolo, int color)
     {
         super(simbolo, color);
@@ -9,23 +8,99 @@ public class Rey extends Pieza{
 
     public boolean moverANuevaPosicion (int turno, int posicionPiezaX, int posicionPiezaY, int nuevaPosicionX,
                                         int nuevaPosicionY, Pieza[][] piezas, Pieza variableNuevaPosicionTemporal, ArrayList<String> piezasMuertas) {
-        Pieza temporalRey = piezas[posicionPiezaX][posicionPiezaY];
-        int posicionOriginalX = posicionPiezaX;
-        int posicionOriginalY = posicionPiezaY;
 
         try {
-            while (temporalRey != piezas[nuevaPosicionX][nuevaPosicionY]) {
-                if (((posicionPiezaX - nuevaPosicionX > 0) && (posicionPiezaY - nuevaPosicionY > 0))
-                        || ((posicionPiezaX - nuevaPosicionX < 0) && (posicionPiezaY - nuevaPosicionY < 0))
-                        || ((posicionPiezaX - nuevaPosicionX < 0) && (posicionPiezaY - nuevaPosicionY > 0))
-                        || ((posicionPiezaX - nuevaPosicionX > 0) && (posicionPiezaY - nuevaPosicionY < 0))) {
-                    System.out.println("Las torres no se pueden mover diagonalmente.");
-                    return true;
-                }
+            /* Condición que verifica la posición deseada de la pieza hacia (x-1)(y-1). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX > 0) && (posicionPiezaY - nuevaPosicionY > 0)
+                    && (piezas[posicionPiezaX - 1][posicionPiezaY - 1].color == 0
+                    || piezas[posicionPiezaX - 1][posicionPiezaY - 1].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
             }
-        } catch (Exception e) {
+            /* Condición que verifica la posición deseada de la pieza hacia (x-1)(y). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX > 0) && (posicionPiezaY - nuevaPosicionY == 0)
+                    && (piezas[posicionPiezaX - 1][posicionPiezaY].color == 0
+                    || piezas[posicionPiezaX - 1][posicionPiezaY].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+            /* Condición que verifica la posición deseada de la pieza hacia (x-1)(y+1). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX > 0) && (posicionPiezaY - nuevaPosicionY < 0)
+                    && (piezas[posicionPiezaX - 1][posicionPiezaY + 1].color == 0
+                    || piezas[posicionPiezaX - 1][posicionPiezaY + 1].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+            /* Condición que verifica la posición deseada de la pieza hacia (x)(y-1). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX == 0) && (posicionPiezaY - nuevaPosicionY > 0)
+                    && (piezas[posicionPiezaX][posicionPiezaY - 1].color == 0
+                    || piezas[posicionPiezaX][posicionPiezaY - 1].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+            /* Condición que verifica la posición deseada de la pieza hacia (x)(y+1). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX == 0) && (posicionPiezaY - nuevaPosicionY < 0)
+                    && (piezas[posicionPiezaX][posicionPiezaY + 1].color == 0
+                    || piezas[posicionPiezaX][posicionPiezaY + 1].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+            /* Condición que verifica la posición deseada de la pieza hacia (x+1)(y-1). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX < 0) && (posicionPiezaY - nuevaPosicionY > 0)
+                    && (piezas[posicionPiezaX + 1][posicionPiezaY - 1].color == 0
+                    || piezas[posicionPiezaX + 1][posicionPiezaY - 1].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+            /* Condición que verifica la posición deseada de la pieza hacia (x+1)(y). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX < 0) && (posicionPiezaY - nuevaPosicionY == 0)
+                    && (piezas[posicionPiezaX + 1][posicionPiezaY].color == 0
+                    || piezas[posicionPiezaX + 1][posicionPiezaY].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+            /* Condición que verifica la posición deseada de la pieza hacia (x+1)(y+1). Si se cumple,
+            entonces la pieza se mueve una casilla en dicha dirección*/
+            if ((posicionPiezaX - nuevaPosicionX < 0) && (posicionPiezaY - nuevaPosicionY < 0)
+                    && (piezas[posicionPiezaX + 1][posicionPiezaY + 1].color == 0
+                    || piezas[posicionPiezaX + 1][posicionPiezaY + 1].color != turno)) {
+                return valorarEspacioEnemigo(turno, posicionPiezaX, posicionPiezaY, nuevaPosicionX, nuevaPosicionY,
+                        piezas, variableNuevaPosicionTemporal, piezasMuertas);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
 
         }
-        return true;
+        System.out.println("El objetivo no es alcanzable por la pieza. (out of bounds)");
+
+        return false;
+    }
+
+    private boolean valorarEspacioEnemigo(int turno, int posicionPiezaX, int posicionPiezaY, int nuevaPosicionX,
+                                          int nuevaPosicionY, Pieza[][] piezas, Pieza variableNuevaPosicionTemporal, ArrayList<String> piezasMuertas)
+    {
+        if (turno == 0){
+            piezas[nuevaPosicionX][nuevaPosicionY]=this;
+            piezas[posicionPiezaX][posicionPiezaY]=variableNuevaPosicionTemporal;
+            return true;
+        }
+        else if (turno != piezas[nuevaPosicionX][nuevaPosicionY].color)
+        {
+            piezas[nuevaPosicionX][nuevaPosicionY]=this;
+            piezasMuertas.add(variableNuevaPosicionTemporal.valor);
+            resetearPieza(variableNuevaPosicionTemporal);
+            piezas[posicionPiezaX][posicionPiezaY]=variableNuevaPosicionTemporal;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
